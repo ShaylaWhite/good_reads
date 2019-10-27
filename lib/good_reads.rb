2ead2618766b4a076bf4ed7bf7a141703df4ae4f
Shayla White <shayladwhite1@gmail.com>
2019-10-27 22:48:39 +0000

    pry

--- lib/good_reads.rb
+++ lib/good_reads.rb
@@ -1,6 +1,8 @@
 require_relative "./good_reads/version"
 require_relative "./good_reads/cli"
 
+require 'pry'
+
 module GoodReads
   class Error < StandardError; end
   # Your code goes here...
--- lib/good_reads/cli.rb
+++ lib/good_reads/cli.rb
@@ -1,11 +1,13 @@
 class GoodReads::CLI
   
-def call
+ def call
     
   puts " " " "
   puts " " "    " " Welcome, Get Ready For Some Good Artsy Reads!"
   puts " " " "
   puts   " " "    " " 'A book is a gift you can open again and again' " 
   puts "" "    " "  " "    " " " "  " "    " " " "  " "    " "  " " " "  " "    " " — Garrison Keillor"
+  
+  binding.pry
    end
 end 
