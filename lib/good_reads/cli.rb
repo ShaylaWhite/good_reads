44a623f1f233b9f2b4609a7732a1916196a8d29c
Shayla White <shayladwhite1@gmail.com>
2019-10-27 23:22:48 +0000

    cli

--- lib/good_reads/cli.rb
+++ lib/good_reads/cli.rb
@@ -1,13 +1,31 @@
-class GoodReads::CLI
-  
- def call
+class GoodReads::CLI 
+  def call
     
-  puts " " " "
-  puts " " "    " " Welcome, Get Ready For Some Good Artsy Reads!"
-  puts " " " "
-  puts   " " "    " " 'A book is a gift you can open again and again' " 
-  puts "" "    " "  " "    " " " "  " "    " " " "  " "    " "  " " " "  " "    " " — Garrison Keillor"
+    puts " " " "
+    puts " " "    " " Welcome, Get Ready For Some Good Artsy Reads!"
+    puts " " " "
+    puts   " " "    " " 'A book is a gift you can open again and again' " 
+
+
+    get_book_title
+    get_user_title_selection
+    
+    #get book_rating
+    #Get book_titles
+    #Get Author_Name or 
+    #Get Book_Name 
+    #User Choose the book title or 
+  end  
   
-  binding.pry
-   end
-end 
+  def get_book_title
+       @title = ['v amy', 'v sam', 'f frank', 'c shayla']
+  end
+    
+    
+  def get_user_title_selection
+                                             #list the title of the books
+      @title.each_with_index do |title, index|
+      puts "#{index + 1}. #{title}"         # +1 to count up
+    end
+  end
+end
