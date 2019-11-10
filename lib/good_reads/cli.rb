346b47f9a7d22cc5c3e6b19c33766c0bd59d7f9e
Shayla White <shayladwhite1@gmail.com>
2019-11-10 22:50:45 +0000

    cli

--- lib/good_reads/cli.rb
+++ lib/good_reads/cli.rb
@@ -2,19 +2,26 @@ require 'colorize'
 class GoodReads::CLI 
   
   def call
-    puts "\n___________________________________________________".underline
-    puts ""
-    puts "\n Hello Readers, get ready for a few Good Art Reads!".white.on_green.bold
-    puts "\n___________________________________________________".underline
+    puts "-------------------------------------------------".underline
+    puts "\n-------Hello My Fellow Art Book Readers!!--------".white.on_magenta 
+    puts "-------Brace Yourself For A Few ARTSY Reads------".white.on_magenta
+    puts "-------------------------------------------------".underline
+    puts "\n"
     list_art_books
     accept_user_selection
     list_book_review
   end  
   
   def list_art_books
+    puts "--------------------------------------------------"
+    puts "-------Here are the newly released ArtBooks-------".white.on_magenta
+    puts "--------------------------------------------------"
   end
   
   def accept_user_selection
+    puts "--------------------------------------------------"
+    puts "-----What's your book selection? Enter the'#'-----".white.on_magenta
+    puts "--------------------------------------------------"
   end
   
   def list_book_review
