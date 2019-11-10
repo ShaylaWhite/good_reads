b0d6e56d03c5bc48f4501e416f02e06a218c69a2
Shayla White <shayladwhite1@gmail.com>
2019-11-10 22:07:44 +0000

    cli

--- lib/good_reads.rb
+++ lib/good_reads.rb
@@ -1,13 +1,10 @@
 require_relative "./good_reads/version"
 require_relative "./good_reads/cli"
-require_relative "./good_reads/title"
+require_relative "./good_reads/review"
 require_relative "./good_reads/scraper"
 
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-module GoodReads
-  class Error < StandardError; end
-  # Your code goes here...
-end
+#acts as the envirmonent that all of my pages can require and in return will have access to all of the code.
--- lib/good_reads/cli.rb
+++ lib/good_reads/cli.rb
@@ -1,5 +1,12 @@
 require 'colorize'
 class GoodReads::CLI 
+  
   def call
-    
+    puts "\n--------------------------------------------------".underline
+    puts "\n Hello Readers, get ready for a few Good Art Reads!".white.on_red
+    puts "\n--------------------------------------------------".underline
+  end  
+  
+  
+  
 end
