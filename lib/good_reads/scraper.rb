00d36eb5d5927d7e810cfebfe7042dc0db00a855
Shayla White <shayladwhite1@gmail.com>
2019-11-13 03:16:59 +0000

    refdfdf

--- lib/good_reads/scraper.rb
+++ lib/good_reads/scraper.rb
@@ -4,19 +4,9 @@ class GoodReads::Scraper
  
   def self.scrape_site
     doc = Nokogiri::HTML(open("https://www.goodreads.com/genres/art"))
-  end
-  
-  #scraping the books off the site
-  
-  def self.scrape_books
     results = self.scrape_site.css("div.bigBoxBody").first
     books = results.css("div.coverWrapper")  
     books
-  end
- 
- #creating a new books and giving each book instance a title and url
-   
-  def self.get_title_url
     
   self.scrape_books.each do |book|
   book = GoodReads::Books.new
